83e91fe1a31bc187bd25a90cb192195465c28055
Alexander Ulrichsen <alexander@peacocktech.co.uk>
2021-12-06 10:23:32 +0000

    Added new example 3 layer ONN

--- models.py
+++ models.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 
 from Self_ONN import Operator_Layer
 
-class Single_Layer_ONN(nn.Module):
+class Single_Layer_Embedding_ONN(nn.Module):
     def __init__(self, in_channels, out_channels, q_order=3, emb_out=512, ks=3, act=nn.Tanh()):
-        super(Single_Layer_ONN, self).__init__()
+        super(Single_Layer_Embedding_ONN, self).__init__()
 
-        self.operator = nn.Operator_Layer(in_channels, out_channels, ks, q_order)
+        self.operator = Operator_Layer(in_channels, out_channels, ks, q_order)
         self.act = act  # Activation fiunction
         self.avg_pool = nn.AvgPool2d((1,1))
         self.embedding = nn.Linear(out_channels, emb_out)
@@ -18,4 +18,24 @@ class Single_Layer_ONN(nn.Module):
         x = self.avg_pool(x)
         x = self.embedding(x)
         x = nn.functional.normalize(x, p=2, dim=1)  # L2 norm
+        return x
+
+
+class Three_Layer_ONN(nn.Module):
+    def __init__(self, in_channels, out_channels, q_order=3, ks=3, act=nn.Tanh()):
+        super(Three_Layer_ONN, self).__init__()
+
+        self.OP_layer_1 = Operator_Layer(in_channels, 64, ks, q_order)
+        self.OP_layer_2 = Operator_Layer(64, 64, ks, q_order)
+        self.OP_layer_3 = Operator_Layer(64, out_channels, ks, q_order)
+        
+        self.act = act  # Activation fiunction
+
+    def forward(self, x):
+        x = self.OP_layer_1(x)
+        x = self.act(x)
+        x = self.OP_layer_2(x)
+        x = self.act(x)
+        x = self.OP_layer_3(x)
+        x = self.act(x)
         return x
