708a5e616f931ae797d78f4a26cb2775ed21cea2
NourO93 <32168830+NourO93@users.noreply.github.com>
2022-02-09 19:52:24 +0400

    Update Self_ONN.py

--- SR/Self_ONN.py
+++ SR/Self_ONN.py
@@ -28,11 +28,7 @@ class Operator_Layer(nn.Module):
 
         #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
 
-        #self.operator = nn.Conv2d(in_channels*q_order, out_channels, ks,padding='same')
-        self.conv1 = nn.Conv2d(in_channels*q_order, 64, kernel_size=9, padding='same')
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding='same')
-        self.conv3 = nn.Conv2d(32, out_channels, kernel_size=5, padding='same')
-        self.relu = nn.ReLU(inplace=True)
+        self.operator = nn.Conv3d(in_channels*q_order, out_channels, ks, padding='same')
 
     def forward(self, x):
         # Raise inputs up to power q 
@@ -41,11 +37,36 @@ class Operator_Layer(nn.Module):
             power_input.append(torch.pow(x, q))
         power_input = torch.cat(power_input, 1)
 
-        #operator_out = self.operator(power_input) #torch.relu(self.operator(power_input),inplace=False)
-        x = self.relu(self.conv1(power_input))
-        x = self.relu(self.conv2(x))
-        operator_out = self.conv3(x)
+        operator_out = self.operator(power_input)
+
         return operator_out
+
+class Operator_Layer_Split(nn.Module):
+    """
+    Operator Layer where each q component is a seperate group so that learning rates can be set individually
+    """
+
+    def __init__(self, in_channels, out_channels, ks: int = 3, q_order: int = 3):
+        """
+        q_order: the MacLaurin series order approximation
+        """
+        super(Operator_Layer_Split, self).__init__()
+
+        self.in_channels = in_channels
+        self.q_order = q_order
+
+        #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
+
+        self.operators = nn.ModuleList([nn.Conv2d(in_channels, out_channels, ks)] + [nn.Conv2d(in_channels, out_channels, ks, bias=False) for _ in range(q_order-1)]    )
+        #self.bias = nn.Parameter(torch.rand(1,out_channels,1,1))
+
+    def forward(self, x):
+        # Raise inputs up to power q 
+        out = self.operators[0](x)
+        for q in range(2, self.q_order+1):
+            out += self.operators[q-1](torch.pow(x, q))
+
+        return out
 
 if __name__ == "__main__":
     ip = torch.ones(2,2,4,4)
