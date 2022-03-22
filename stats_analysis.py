53e7feb7811ecaadbea27ccae38cdd5f6e6dd0a5
Alexander Ulrichsen <alexander@peacocktech.co.uk>
2022-03-22 15:42:39 +0200

    Optimisation statistics analysis plots.

---
+++ stats_analysis.py
@@ -0,0 +1,66 @@
+import argparse
+
+import matplotlib.pyplot as plt
+
+from utils import loadSamples
+
+"""
+Stats file structure: list of dicts where each dict contains a givens sample's statistics
+    Sample's statistics: 
+        "Sample" : Sample index
+        "X" : Hyperparameters selected
+        "Best PSNR" : Best PSNR value of given iteration (across all X runs)
+        "PSNRs" : Validation PSNR values across all epochs (for best run)
+        "SSIMS" : Validation SSIM values across all epochs (for best run)
+        "SAMs" : Validation SAM values across all epochs (for best run)
+"""
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, default='SRCNN_model_samples.json', help="File name of optimisation stats.")
+
+    opt = parser.parse_args()
+    #print_args(F)
+
+    return opt
+
+if __name__ == '__main__':
+    opt = parse_opt()
+
+    samples = loadSamples(opt.file)
+
+    best_PSNRs = []
+    best_sample = 1
+
+    for samp in samples:
+        if best_PSNRs:
+            if samp["Best PSNR"] > max(best_PSNRs):
+                best_sample = samp["Sample"]
+
+        best_PSNRs.append(samp["Best PSNR"])
+
+    print("Best PSNR on sample:", best_sample)
+
+    x = [i+1 for i in range(len(best_PSNRs))]
+    plt.plot(x, best_PSNRs)
+    plt.xlabel("Sample")
+    plt.ylabel("PSNR")
+    plt.title("Best PSNR Values per Sample")
+    plt.show()
+
+    best_sample_PSNRs = samples[best_sample-1]["PSNRs"]
+    best_sample_SSIMs = samples[best_sample-1]["SSIMs"]
+    best_sample_SAMs = samples[best_sample-1]["SAMs"]
+
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3)
+    fig.suptitle(f'Sample {best_sample} Validation Metric Plots')
+    ax1.plot(best_sample_PSNRs)
+    ax1.set_ylabel("PSNR")
+    ax2.plot(best_sample_SSIMs)
+    ax2.set_ylabel("SSIM")
+    ax3.plot(best_sample_SAMs)
+    ax3.set_ylabel("SAM")
+    ax3.set_xlabel("Epoch")
+
+    plt.show()
--- unit_tests/test_Load_Data.py
+++ unit_tests/test_Load_Data.py
@@ -1,12 +1,18 @@
 import unittest
 
 import numpy as np
+import torch.nn as nn
 
 from Load_Data import get_pavia_data, bicubic_lr
 
 class Test_Load_Data(unittest.TestCase):
 
     def test_bicubic_lr(self):
+        """
+        test dimenstion of data produced by bicubic_lr function 
+        test data produced is not identical to input
+        """
+
         test_img = np.random.rand(103,64,64)
 
         test_out = bicubic_lr(test_img, 2)
@@ -16,3 +22,22 @@ class Test_Load_Data(unittest.TestCase):
         mse = ((test_img - test_out)**2).mean(axis=None)
 
         self.assertGreater(mse, 0, "bicubic_lr input and output identical")
+
+    def test_reproducible(self):
+        """
+        Test if data produced by get_pavia_data function is reproducible
+        """
+
+        x1_train, y1_train, x1_val, y1_val, x1_test, y1_test = get_pavia_data()
+
+        x2_train, y2_train, x2_val, y2_val, x2_test, y2_test = get_pavia_data()
+
+        mse_loss = nn.MSELoss()     # MSE function to compare sperate instances of data are identical
+
+        self.assertEqual(mse_loss(x1_train, x2_train), 0, msg="x_train not reproducible.")
+        self.assertEqual(mse_loss(y1_train, y2_train), 0, msg="y_train not reproducible.")
+        self.assertEqual(mse_loss(x1_val, x2_val), 0, msg="x_val not reproducible.")
+        self.assertEqual(mse_loss(y1_val, y2_val), 0, msg="x_train not reproducible.")
+        self.assertEqual(mse_loss(x1_test, x2_test), 0, msg="x_train not reproducible.")
+        self.assertEqual(mse_loss(y1_test, y2_test), 0, msg="x_train not reproducible.")
+
