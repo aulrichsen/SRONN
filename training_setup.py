ea1d06fc70332298413ba2ebdb5acf806c563c1c
Alexander Ulrichsen <alexander@peacocktech.co.uk>
2022-05-06 16:51:13 +0300

    disp_slices b fix.

--- training_setup.py
+++ training_setup.py
@@ -58,6 +58,6 @@ def get_disp_slices(slices="default"):
     if slices == "All":
         disp_slices = [{'b': 94, 'c': 82}, {'b': 55, 'c': 0}, {'b': 126, 'c': 56}, {'b': 28, 'c': 98}, {'b': 88, 'c': 61}, {'b': 125, 'c': 74}]
     else:
-        disp_slices = [{'b': 0, 'c': 0}, {'b': 20, 'c': 20}, {'b': 30, 'c': 30}, {'b': 40, 'c': 40}, {'b': 50, 'c': 50}, {'b': 60, 'c': 60}]
+        disp_slices = [{'b': 0, 'c': 0}, {'b': 2, 'c': 20}, {'b': 3, 'c': 30}, {'b': 4, 'c': 40}, {'b': 5, 'c': 50}, {'b': 6, 'c': 60}]
 
     return disp_slices
